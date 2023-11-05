70bd26095962054d917ff3194479859e7c5da7b9
m.sboskabadi <m.sboskabadi@gmail.com>
2023-11-05 20:45:28 +0330

    add eager loading sample to source

---
+++ DataAccessLayer/CourseStoreRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class CourseStoreRepository
+    {
+        private CourseStoredDbContext _courseStoreDbContext;
+
+        public CourseStoreRepository(CourseStoredDbContext courseStoredDbContext)
+        {
+            _courseStoreDbContext = courseStoredDbContext;
+        }
+
+        public void PrintCourseAndTeacher()
+        {
+            var result = _courseStoreDbContext.Courses.Include(c => c.CourseTeachers).ThenInclude(c => c.Teacher).ToList();  
+            foreach(var course in result)
+            {
+                Console.WriteLine($"Course : {course.Name}");
+
+                foreach(var item in course.CourseTeachers)
+                {
+                    Console.WriteLine($"\t\t {item.Teacher.FirstName}");
+                }
+            }
+        }
+    }
+}
--- UI/Program.cs
+++ UI/Program.cs
@@ -5,7 +5,10 @@ using Microsoft.EntityFrameworkCore;
 
 var optionBuilder = new DbContextOptionsBuilder<CourseStoredDbContext>();
 optionBuilder.UseSqlServer("Server=.; Database= CourseShopDb; User Id=sa; Password=0910173532; Encrypt=False");
-
+optionBuilder.LogTo(Console.WriteLine);
 using CourseStoredDbContext ctx = new CourseStoredDbContext(optionBuilder.Options);
 
-var course = ctx.Courses.FirstOrDefault();
+CourseStoreRepository repository = new CourseStoreRepository(ctx);
+repository.PrintCourseAndTeacher();
+
+Console.ReadKey();
